88b5025ab5daf2a22828788595c3ee753895ed8b
Oliver Bruendler <oliver.bruendler@psi.ch>
2019-09-26 09:27:33 +0200

    DEVEL: Added CI scripts

---
+++ scripts/ciFlow.py
@@ -0,0 +1,26 @@
+##############################################################################
+#  Copyright (c) 2018 by Paul Scherrer Institute, Switzerland
+#  All rights reserved.
+#  Authors: Oliver Bruendler
+##############################################################################
+import os
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+os.chdir(THIS_DIR + "/../sim")
+
+os.system("vsim -batch -do ci.do -logfile Transcript.transcript")
+
+with open("Transcript.transcript") as f:
+	content = f.read()
+	
+#Expected Errors
+if "###ERROR###" in content:
+	exit(-1)
+#Unexpected Errors
+if "SIMULATIONS COMPLETED SUCCESSFULLY" not in content:
+	exit(-2)
+	
+	
+#Success
+exit(0)
